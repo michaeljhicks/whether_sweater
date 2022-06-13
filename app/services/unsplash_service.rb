bf5cf582b0dbea709f9d77eee4c51cc89251fb77
Michael Hicks <hicks.michael04@gmail.com>
2022-06-12 20:29:00 -0600

    add unsplash service

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,12 @@
+class UnsplashService
+    def self.conn 
+        Faraday.new(url: 'https://api.unsplash.com') do |f|
+            f.params['client_id'] = ENV['unsplash_access_key']
+        end         
+    end
+
+    def self.location_photo(city)
+        response = conn.get("/search/photos?page=1&query=#{city}")
+        JSON.parse(response.body, symbolize_names: true)
+    end
+end
